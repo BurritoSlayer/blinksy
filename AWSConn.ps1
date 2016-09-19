0271bfeec49daccd5747621b7be7fd74b07ee123
unknown <jeebus92@hotmail.com>
2016-09-19 10:39:26 -0700

    removing hardcoded dir structure

--- AWSConn.ps1
+++ AWSConn.ps1
@@ -1,6 +1,4 @@
 Write-Output "Hello from AWSConn.ps1";
 "Script has been triggered"
 
-#$logFile = 'C:\Users\ntacey\Documents\blinksy\log\blinksyLog.txt'
-
 #"Script has been triggered" | Add-Content $logFile
